10cff5f1f2114943eb59511f8103fd5b354ecfc3
okanr <okan.rn@outlook.de>
2025-03-18 18:43:49 +0100

    add func call

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(".usp_contain").each(function (index) {
     let childTriggers = $(this).find(".usp_content_text-item");
     let childTargets = $(this).find(".usp_visual_img-item");
-
     // switch active class
     function makeItemActive(index) {
         childTriggers.removeClass('is-active');
@@ -9,4 +8,19 @@ $(".usp_contain").each(function (index) {
         childTriggers.eq(index).addClass('is-active');
         childTargets.eq(index).addClass('is-active')
     }
-})
+    makeItemActive(0);
+    // create triggers for each of these items in the list
+    childTriggers.each(function (index) {
+        ScrollTrigger.create({
+            trigger: $(this),
+            start: "top center",
+            end: "bottom center",
+            markers: true,
+            onToggle: (isActive) => {
+                if(isActive) {
+                    makeItemActive(index)
+                }
+            }
+        });
+    });
+});
