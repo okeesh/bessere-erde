3104b2d7967be4dfb99ac86de2f6172e9ee9ef71
okanr <okan.rn@outlook.de>
2025-03-18 18:17:27 +0100

    add func

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+$(".usp_contain").each(function (index) {
+    let childTriggers = $(this).find(".usp_content_text-item");
+    let childTargets = $(this).find(".usp_visual_img-item");
+
+    // switch active class
+    function makeItemActive(index) {
+        childTriggers.removeClass('is-active');
+        childTargets.removeClass('is-active');
+        childTriggers.eq(index).addClass('is-active');
+        childTargets.eq(index).addClass('is-active')
+    }
+})
